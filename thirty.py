9c8b39514a90283177e9b82323e4b37e4b7805d6
Mathias Rav <rav@cs.au.dk>
2016-08-07 15:18:10 +0200

    Implement something that seems correct

---
+++ thirty.py
@@ -0,0 +1,97 @@
+import operator
+import fractions
+import itertools
+import functools
+import collections
+
+
+def product(iterable):
+    return functools.reduce(operator.mul, iterable, 1)
+
+
+def factorial(n):
+    """
+    >>> [factorial(n) for n in range(6)]
+    [1, 1, 2, 6, 24, 120]
+    """
+    return product(range(1, n+1))
+
+
+def permutations(s):
+    """
+    >>> permutations('cat')
+    6
+    >>> permutations('mom')
+    3
+    """
+    counts = collections.Counter(s)
+    n = sum(counts.values())
+    return factorial(n) // product(map(factorial, counts.values()))
+
+
+def value(dice_count, sides, utility):
+    """
+    Suppose we have n k-sided dice (sides 0, 1, ..., k-1)
+    and we perform the following process:
+    Throw the dice, and take out a non-empty subset of them,
+    remembering the sum.  As long as you still have dice left, throw the
+    remaining dice, and take out a non-empty subset of them, adding up their
+    sum with what you already put aside.
+    When you have no more dice, you have a resulting sum (between 0 and n*(k-1))
+    and you win utility(sum).
+    What is the expected utility of the optimal strategy?
+
+    >>> print(value(1, 6, lambda s: s))  # Expected throw
+    5/2
+
+    Probability of getting an even number:
+    >>> print(value(1, 6, lambda s: 1 if s % 2 == 0 else 0))
+    1/2
+    """
+
+    # values[n][s] == v means that for n remaining dice,
+    # accumulated sum s, the expected utility is v.
+    values = []
+
+    # Fill out "values" for n = 0 using the utility function.
+    values.append([utility(s) for s in range(dice_count * (sides-1) + 1)])
+
+    # How do you divide two numbers?
+    div = fractions.Fraction
+
+    for n in range(1, dice_count+1):
+        # What might the accumulated sum be at most with n dice remaining?
+        max_sum = (dice_count - n) * (sides - 1)
+        # At the end, tmp_value[s] will be k**n times the expected utility.
+        tmp_value = [0 for s in range(max_sum + 1)]
+        # What can we do with an outcome?
+        # Reroll the first m (0 <= m < n) or the last m (1 <= m < n).
+        rerolls = ([slice(0, m) for m in range(n)] +
+                   [slice(m, n) for m in range(1, n)])
+
+        outcomes = itertools.combinations_with_replacement(range(sides), n)
+        n_outcomes = 0
+        for outcome in outcomes:
+            outcome_sum = sum(outcome)
+            multiplicity = permutations(outcome)
+            n_outcomes += multiplicity
+            for s in range(0, max_sum + 1):
+                best = min(values[0])
+                for reroll_slice in rerolls:
+                    reroll = outcome[reroll_slice]
+                    reroll_sum = sum(reroll)
+                    keep_sum = outcome_sum - reroll_sum
+                    # Suppose we had already accumulated "s",
+                    # and now we keep another "keep_sum"
+                    # and reroll the "reroll" dice.
+                    best = max(best, values[len(reroll)][s + keep_sum])
+                tmp_value[s] += multiplicity * best
+
+        assert n_outcomes == sides ** n
+
+        values.append([div(a, n_outcomes) for a in tmp_value])
+    return values[dice_count][0]
+
+
+if __name__ == "__main__":
+    print(value(2, 6, lambda s: 1 if s == 10 else 0))
