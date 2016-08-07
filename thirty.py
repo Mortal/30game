39c7b6757e1a750d66eaba29d010d2fd43262637
Mathias Rav <rav@cs.au.dk>
2016-08-07 16:21:52 +0200

    Add function to describe action

--- thirty.py
+++ thirty.py
@@ -154,6 +154,28 @@ def my_utility(s):
         return above[s - 25]
 
 
+def describe_keep_reroll(dice_count, sides, strategy, roll, s):
+    roll_z = [v - 1 for v in roll]
+    s_z = s - (dice_count - len(roll))
+    reroll_z = strategy(roll_z, s_z)[0]
+    reroll = [v + 1 for v in reroll_z]
+
+    roll_hist = collections.Counter(roll)
+    reroll_hist = collections.Counter(reroll)
+    keep_hist = roll_hist - reroll_hist
+    keep = sorted(keep_hist.elements())
+    if reroll == []:
+        return 'stop'
+    elif len(keep) == 1:
+        return 'keep a %s' % keep[0]
+    elif len(reroll) == 1:
+        return 'reroll a %s' % reroll[0]
+    elif len(keep) < len(reroll):
+        return 'keep %s' % ' '.join(map(str, keep))
+    else:
+        return 'reroll %s' % ' '.join(map(str, reroll))
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--infiniplay', action='store_true')
